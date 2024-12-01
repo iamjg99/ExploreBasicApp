35a14b3ab9577219d11da1e4e6941865d14d71b5
jatin-iOS <jatin.gupta@eazydiner.com>
2024-12-01 18:06:55 +0530

    Initial Commit

---
+++ EvolveAssignment/EvolveAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvolveAssignmentApp.swift
+//  EvolveAssignment
+//
+//  Created by Jatin Gupta on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EvolveAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
